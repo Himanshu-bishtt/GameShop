92561a535a786cdc8204e8410490ed3b71a6c89b
Himanshu Bisht <himanshubisht2255@gmail.com>
2021-03-29 13:17:45 +0530

    changed css

--- dist/js/main.js
+++ dist/js/main.js
@@ -79,14 +79,17 @@ document.querySelector(".topbar").addEventListener("click", function (e) {
 window.addEventListener("scroll", function () {
   let navbar = document.querySelector("#main-header");
   let topbar = document.querySelector(".topbar");
-  // let navbarInner = document.querySelector(".navbarInner");
+  let navbarActions = document.querySelector(".actions");
+  let navbarLogo = document.querySelector(".logo");
   if (window.scrollY > 600) {
     navbar.style.opacity = 0.95;
     topbar.style.display = "none";
+    navbarActions.style.display = "none";
+    navbarLogo.style.display = "block";
   } else {
     navbar.style.opacity = 1;
-
-    // topbar.style.display = "block";
+    navbarActions.style.display = "flex";
+    navbarLogo.style.display = "none";
   }
 });
 
